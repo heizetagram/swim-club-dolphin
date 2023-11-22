a7d7f0628a2a4154d7ba3d383acc6c99cc525635
bigcheesespaghett <134272992+bigcheesespaghett@users.noreply.github.com>
2023-11-22 14:19:19 +0100

    Refactored method names, Added SystemRunning

--- security/Login.java
+++ security/Login.java
@@ -1,27 +1,26 @@
 package security;
 
 import menu.ChooseMenuOption;
-import harryssalon.Main;
 import menu.ShowMenu;
+import system.SystemRunning;
 import ui.UI;
 
 public class Login {
-    private Main main;
+
     private ShowMenu showMenu;
     private ChooseMenuOption chooseMenuOption;
     private Password password;
 
     // Constructor
-    public Login(Main main) {
-        this.main = main;
+    public Login() {
         password = new Password();
         showMenu = new ShowMenu();
-        chooseMenuOption = new ChooseMenuOption(main);
+        chooseMenuOption = new ChooseMenuOption();
     }
 
     // Customer
     public void loginTrainer() {
-        while (main.isRunning()) { // Keep showing Customer's menu while logged in
+        while (SystemRunning.isRunning()) { // Keep showing Customer's menu while logged in
             showMenu.showTrainerMenu();
             chooseMenuOption.chooseTrainerMenuOption();
         }
@@ -32,7 +31,7 @@ public class Login {
         UI.println("Enter password");
         UI.promptString(); // Scanner bug
         password.checkPassword(UI.promptString(), "1");
-        while (password.isPasswordCorrect() && main.isRunning()) { // Keep showing Harry's menu while logged in
+        while (password.isPasswordCorrect() && SystemRunning.isRunning()) { // Keep showing Harry's menu while logged in
             showMenu.showForemanMenu();
             chooseMenuOption.chooseForemanMenuOption();
         }
@@ -46,12 +45,12 @@ public class Login {
         UI.println("Enter password");
         UI.promptString(); // Scanner bug
         password.checkPassword(UI.promptString(), "1");
-        while (password.isPasswordCorrect() && main.isRunning()) { // Keep showing Accountant's menu while logged in
+        while (password.isPasswordCorrect() && SystemRunning.isRunning()) { // Keep showing Accountant's menu while logged in
             showMenu.showAccountantMenu();
             chooseMenuOption.chooseAccountantMenuOption();
         }
         if (!password.isPasswordCorrect()) {
-            main.setRunning(false);
+            SystemRunning.setRunning(false);
         }
     }
 }
