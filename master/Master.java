1e53636cda0edd4ef0935e0f520722d9ecf3981d
bigcheesespaghett <134272992+bigcheesespaghett@users.noreply.github.com>
2023-11-22 14:23:19 +0100

    Imported classes

--- master/Master.java
+++ master/Master.java
@@ -1,12 +1,17 @@
 package master;
 
 import filehandling.FileHandling;
+import menu.ChooseMenuOption;
+import menu.ShowMenu;
+import system.SystemRunning;
 
 public class Master {
     private FileHandling fileHandling;
+    private ChooseMenuOption chooseMenuOption;
 
     private void initVars() {
         fileHandling = new FileHandling();
+        chooseMenuOption = new ChooseMenuOption();
     }
 
     public static void main(String[] args) {
