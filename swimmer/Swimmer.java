eee4f11b2163561bde819d067a18badc2b29a769
bigcheesespaghett <134272992+bigcheesespaghett@users.noreply.github.com>
2023-11-22 14:25:03 +0100

    Added birthdate, made phone int, added getters and setters

--- swimmer/Swimmer.java
+++ swimmer/Swimmer.java
@@ -5,18 +5,19 @@ import java.util.ArrayList;
 public class Swimmer {
     private ArrayList<Swimmer> swimmers;
     private String name;
-    private int birthYear;
+    private String birthdate;
+    private int phone;
     private String email;
-    private String phone;
 
-    public Swimmer(String name, int birthYear, String email, String phone) {
+    public Swimmer(String name, String birthdate, int phone, String email) {
         swimmers = new ArrayList<>();
         this.name = name;
-        this. birthYear = birthYear;
-        this.email = email;
+        this.birthdate = birthdate;
         this.phone = phone;
+        this.email = email;
     }
 
+    // Getters
     public ArrayList<Swimmer> getSwimmers() {
         return swimmers;
     }
@@ -25,15 +26,37 @@ public class Swimmer {
         return name;
     }
 
-    public int getBirthYear() {
-        return birthYear;
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public int getPhone() {
+        return phone;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public String getPhone() {
-        return phone;
+
+    // Setters
+    public void setSwimmers(ArrayList<Swimmer> swimmers) {
+        this.swimmers = swimmers;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
