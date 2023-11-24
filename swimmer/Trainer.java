abbd2977eb123ef85ab1e54e9f40b272b2e1fe3e
David Lu <davidlu1999@gmail.com>
2023-11-24 12:56:49 +0100

    Added Trainer class

---
+++ swimmer/Trainer.java
@@ -0,0 +1,11 @@
+package swimmer;
+
+import java.util.ArrayList;
+
+public class Trainer {
+    private String name;
+    private String description; // Shows which team
+    private ArrayList<CompetitiveSwimmer> team;
+
+
+}
