2b3aa5ee12b5989b5ac752dc25dc00db23c0ce28
David Lu <davidlu1999@gmail.com>
2023-11-24 12:56:40 +0100

    Added pseudocode

--- swimmer/Team.java
+++ swimmer/Team.java
@@ -1,5 +1,26 @@
 package swimmer;
 
+import java.util.ArrayList;
+
 public class Team {
-    private boolean isJunior;
+    private ArrayList<Swimmer> juniorTeam;
+    private ArrayList<Swimmer> seniorTeam;
+
+    public ArrayList<Swimmer> getJuniorTeam() {
+        return juniorTeam;
+    }
+
+    public ArrayList<Swimmer> getSeniorTeam() {
+        return seniorTeam;
+    }
+
+    /* Tilføj til korrekt klasse
+    private void testtest(int age, Swimmer swimmer) {
+        if (age > 18) {
+            getSeniorTeam().add(swimmer);
+        } else
+            getJuniorTeam().add(swimmer);
+    }
+
+     */
 }
