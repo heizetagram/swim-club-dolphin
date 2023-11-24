d06466c5661a408a75ced5a53ca5ece471883b02
bigcheesespaghett <134272992+bigcheesespaghett@users.noreply.github.com>
2023-11-24 16:38:43 +0100

    Added checkIfDateIsValid(), convertBirthdateToLocalDateTime(), checkIfUnderage()

--- swimmer/Swimmer.java
+++ swimmer/Swimmer.java
@@ -1,27 +1,58 @@
+
 package swimmer;
 
-import java.util.ArrayList;
+import java.time.LocalDateTime;
+import ui.SystemMessages;
+import java.time.DateTimeException;
+import java.time.temporal.ChronoUnit;
 
 public class Swimmer {
     private String name;
     private String birthdate;
-    private int phone;
+    private String phone;
     private String email;
-    private boolean isJunior;
+    private int age;
+    private String isJunior;
 
-    public Swimmer(String name, String birthdate, int phone, String email) {
-        swimmers = new ArrayList<>();
+    public Swimmer(String name, String birthdate, String phone, String email) {
         this.name = name;
         this.birthdate = birthdate;
         this.phone = phone;
         this.email = email;
-        //isJunior = checkIfUnderage(age);
+        isJunior = checkIfUnderage(age);
     }
 
-    /*
-    private boolean checkIfUnderage(int age) {
+    public boolean checkIfDateIsValid(String[] parts) {
+        PromptSwimmer promptSwimmer = new PromptSwimmer();
+        boolean running = true;
+        try {
+            // --calculate age in another method
+            LocalDateTime localDateTimeBirthdate = convertBirthdateToLocalDateTime(parts);
+            age = calculateAge(localDateTimeBirthdate);
+            running = false;
+        } catch (DateTimeException e) {
+            SystemMessages.printRedColoredText("Date doesn't exist");
+            SystemMessages.tryAgain();
+            promptSwimmer.promptBirthdate();
+        }
+        return running;
     }
-     */
+
+    // Converts String birthdate to LocalDateTime
+    private LocalDateTime convertBirthdateToLocalDateTime(String[] parts) {
+        int day = Integer.parseInt(parts[0]);
+        int month = Integer.parseInt(parts[1]);
+        int year = Integer.parseInt(parts[2]);
+
+        return LocalDateTime.of(year, month, day, 0, 0);
+    }
+
+    // Calculates age
+    // Subtracts user-given birthdate from the current date
+    private int calculateAge(LocalDateTime birthdate) {
+        LocalDateTime currentDate = LocalDateTime.now();
+        return (int) ChronoUnit.YEARS.between(birthdate, currentDate); // Typecasts long to int
+    }
 
 
     // Getters
@@ -37,12 +68,20 @@ public class Swimmer {
         return birthdate;
     }
 
-    public int getPhone() {
+    public String getPhone() {
         return phone;
     }
 
     public String getEmail() {
         return email;
+    }
+
+    public String getIsJunior() {
+        return isJunior;
+    }
+
+    public int getAge() {
+        return age;
     }
 
 
@@ -54,11 +93,20 @@ public class Swimmer {
         this.birthdate = birthdate;
     }
 
-    public void setPhone(int phone) {
+    public void setPhone(String phone) {
         this.phone = phone;
     }
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String checkIfUnderage(int age) {
+        if (age > 18) {
+            isJunior = "SENIOR";
+        } else {
+            isJunior = "JUNIOR";
+        }
+        return isJunior;
     }
 }
