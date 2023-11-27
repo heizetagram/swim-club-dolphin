e20da5568afb89c6d896ccb783e593494107149b
David Lu <davidlu1999@gmail.com>
2023-11-27 15:53:04 +0100

    Added loadCompetitiveSwimmerFromFile()

--- master/Master.java
+++ master/Master.java
@@ -14,6 +14,7 @@
         private void initVars() {
             fileHandling = new FileHandling();
             fileHandling.loadSwimmerFromFile();
+            fileHandling.loadCompetitiveSwimmerFromFile();
 
             chooseMenuOption = new ChooseMenuOption();
             SystemRunning.setRunning(true);
