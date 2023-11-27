eb1eb6607fe3248e1d76bbd785266d4ab2ce1421
David Lu <davidlu1999@gmail.com>
2023-11-27 15:48:18 +0100

    Made Discipline a String

--- swimmer/CompetitiveSwimmer.java
+++ swimmer/CompetitiveSwimmer.java
@@ -1,11 +1,13 @@
 package swimmer;
 
+import trainer.Trainer;
+
 public class CompetitiveSwimmer extends Swimmer {
-    private Discipline discipline;
+    private String discipline;
     private String swimTime;
     private Trainer trainer;
 
-    public CompetitiveSwimmer(String name, String birthday, String phone, String email, Discipline discipline, String swimTime) {
+    public CompetitiveSwimmer(String name, String birthday, String phone, String email, String discipline, String swimTime) {
         super(name, birthday, phone, email);
         this.discipline = discipline;
         this.swimTime = swimTime;
