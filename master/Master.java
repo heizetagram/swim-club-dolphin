c2224d67e425d62a5596cc7233b2d970967b5dcd
David Lu <davidlu1999@gmail.com>
2023-11-29 23:06:01 +0100

    Reformatted

--- master/Master.java
+++ master/Master.java
@@ -1,44 +1,44 @@
-    package master;
+package master;
 
-    import filehandling.FileHandling;
-    import menu.ChooseMenuOption;
-    import menu.ShowMenu;
-    import swimmer.CalculateSwimmerAge;
-    import system.SystemRunning;
-    import trainer.SortSwimmers;
+import filehandling.FileHandling;
+import menu.ChooseMenuOption;
+import menu.ShowMenu;
+import swimmer.CalculateSwimmerAge;
+import system.SystemRunning;
+import trainer.SortSwimmers;
 
-    public class Master {
-        private FileHandling fileHandling;
-        private CalculateSwimmerAge calculateSwimmerAge;
-        private CalculateSwimmerAge calculateCompetitiveSwimmerAge;
-        private ChooseMenuOption chooseMenuOption;
+public class Master {
+    private FileHandling fileHandling;
+    private CalculateSwimmerAge calculateSwimmerAge;
+    private CalculateSwimmerAge calculateCompetitiveSwimmerAge;
+    private ChooseMenuOption chooseMenuOption;
 
-        private void initVars() {
-            calculateSwimmerAge = new CalculateSwimmerAge();
-            calculateCompetitiveSwimmerAge = new CalculateSwimmerAge();
-            fileHandling = new FileHandling();
+    private void initVars() {
+        calculateSwimmerAge = new CalculateSwimmerAge();
+        calculateCompetitiveSwimmerAge = new CalculateSwimmerAge();
+        fileHandling = new FileHandling();
 
-            chooseMenuOption = new ChooseMenuOption();
-            SystemRunning.setRunning(true);
-        }
+        chooseMenuOption = new ChooseMenuOption();
+        SystemRunning.setRunning(true);
+    }
 
-        public static void main(String[] args) {
-            new Master().run();
-        }
+    public static void main(String[] args) {
+        new Master().run();
+    }
 
-        private void run() {
-            initVars();
+    private void run() {
+        initVars();
 
-            calculateSwimmerAge.setSwimmersAge(fileHandling);
-            calculateCompetitiveSwimmerAge.setCompetitiveSwimmersAge(fileHandling);
+        calculateSwimmerAge.setSwimmersAge(fileHandling);
+        calculateCompetitiveSwimmerAge.setCompetitiveSwimmersAge(fileHandling);
 
-            SortSwimmers sortSwimmers = new SortSwimmers(fileHandling);
-            sortSwimmers.compareDisciplineAndSwimTime();
-            sortSwimmers.printTop5CompetitiveSwimmers();
+        SortSwimmers sortSwimmers = new SortSwimmers(fileHandling);
+        sortSwimmers.compareDisciplineAndSwimTime();
+        sortSwimmers.printTop5CompetitiveSwimmers();
 
-            while (SystemRunning.isRunning()) {
-                ShowMenu.showRoleSelection();
-                chooseMenuOption.chooseRoleSelectionMenuOption();
-            }
+        while (SystemRunning.isRunning()) {
+            ShowMenu.showRoleSelection();
+            chooseMenuOption.chooseRoleSelectionMenuOption();
         }
     }
+}
