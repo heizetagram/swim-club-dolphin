0814200e20a99dfc6e356ea34b7d5ff61b59e7b8
Haku95 <142887229+Haku95@users.noreply.github.com>
2023-11-30 12:26:21 +0100

    Accountant class works together with CalculateSwimmerAge

--- accountant/Accountant.java
+++ accountant/Accountant.java
@@ -1,12 +1,18 @@
 package accountant;
 
+import swimmer.CalculateSwimmerAge;
+import swimmer.Swimmer;
+
 public class Accountant {
     // membership fee based on the member's age and activity type.
     public double calculateMembershipFee(int age, String activityType) {
         double baseFee = 0.0;
-
+        Swimmer swimmer = new Swimmer("", "", "", "");
+        swimmer.setAge(age);
+        CalculateSwimmerAge calculateSwimmerAge = new CalculateSwimmerAge();
+        String ageGroup = calculateSwimmerAge.calculateAgeGroup(age);
         if (activityType.equalsIgnoreCase("active")) {
-            if (age < 18) {
+            if (ageGroup.equalsIgnoreCase("JUNIOR")) {
                 baseFee = 1000.0; // Junior active fee
             } else {
                 baseFee = 1600.0; // Senior active fee
@@ -15,9 +21,9 @@ public class Accountant {
             baseFee = 500.0; // Passive fee
         }
 
-        // discounts for members over 60 years on the senior rate.
+        // discounts for members over 60 years(ELDERLY) on the senior rate.
         if (age > 60 && activityType.equalsIgnoreCase("active")) {
-            baseFee *= 0.75; // Apply 25% discount for seniors on the senior active rate
+            baseFee *= 0.75; // Apply 25% discount for Elderly (60 or more) on the senior active rate
         }
 
         return baseFee;
