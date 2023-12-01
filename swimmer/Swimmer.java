848641c66776075e7524c5d9c3f27ace7cf8df33
David Lu <davidlu1999@gmail.com>
2023-12-01 09:28:08 +0100

    Added new activityType and hasPaid

--- swimmer/Swimmer.java
+++ swimmer/Swimmer.java
@@ -8,12 +8,16 @@ public class Swimmer {
     private String email;
     private int age;
     private String ageGroup;
+    private boolean activityType;
+    private boolean hasPaid;
 
     public Swimmer(String name, String birthdate, String phone, String email) {
         this.name = name;
         this.birthdate = birthdate;
         this.phone = phone;
         this.email = email;
+        activityType = true;
+        hasPaid = false;
     }
 
     // Getters
@@ -35,6 +39,21 @@ public class Swimmer {
     public String getAgeGroup() {
         return ageGroup;
     }
+    public boolean getActivityType() {
+        return activityType;
+    }
+    public String getActivityTypeAsString() {
+        String activityType;
+        if (this.activityType) {
+            activityType = "ACTIVE";
+        } else {
+            activityType = "PASSIVE";
+        }
+        return activityType;
+    }
+    public boolean getHasPaid() {
+        return hasPaid;
+    }
 
     // Setters
     public void setName(String name) {
@@ -54,5 +73,17 @@ public class Swimmer {
     }
     public void setAgeGroup(String ageGroup) {
         this.ageGroup = ageGroup;
+    }
+    public void setActivityType(boolean activityType) {
+        this.activityType = activityType;
+    }
+    public void setActivityTypeStringToBoolean(String activityTypeAsString) {
+        switch (activityTypeAsString) {
+            case "ACTIVE" -> activityType = true;
+            case "PASSIVE" -> activityType = false;
+        }
+    }
+    public void setHasPaid(boolean hasPaid) {
+        this.hasPaid = hasPaid;
     }
 }
