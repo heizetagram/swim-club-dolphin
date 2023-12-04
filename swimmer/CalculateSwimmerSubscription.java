826b47de61a55b90cbbc6c8aef39a0f14b80c12e
David Lu <davidlu1999@gmail.com>
2023-12-04 23:20:56 +0100

    Added setSwimmersSubscription and setCompetitiveSwimmersSubscription

--- swimmer/CalculateSwimmerSubscription.java
+++ swimmer/CalculateSwimmerSubscription.java
@@ -3,20 +3,38 @@ package swimmer;
 import filehandling.FileHandling;
 
 public class CalculateSwimmerSubscription {
-// calculates swimmer subscription based on agegroup
-public int calculateSubscriptionFee(Swimmer swimmer) {
-    return switch (swimmer.getAgeGroup()) {
-        case "JUNIOR" -> 50;
-        case "SENIOR" -> 150;
-        case "ELDERLY" -> 100;
-        default -> 0;
-    };
-}
+    // Calculates swimmer subscription based on agegroup
+    private int calculateSubscriptionFee(Swimmer swimmer) {
+        return switch (swimmer.getAgeGroup()) {
+            case "JUNIOR" -> 1000;
+            case "SENIOR" -> 1600;
+            case "ELDERLY" -> (int) (1600 * 0.75);
+            default -> 0;
+        };
+    }
+
+    // Sets regular swimmer's subscription price
     public void setSwimmersSubscription(FileHandling fileHandling) {
         for (Swimmer swimmer : fileHandling.getSwimmers()) {
-            int subscriptionFee = calculateSubscriptionFee(swimmer);
-            swimmer.setSubscriptionFee(subscriptionFee);
+            if (swimmer.getActivityType()) {
+                int subscriptionFee = calculateSubscriptionFee(swimmer);
+                swimmer.setSubscriptionFee(subscriptionFee);
+            } else {
+                swimmer.setSubscriptionFee(500);
+            }
         }
     }
 
+      // Sets competitive swimmer's subscription price
+    public void setCompetitiveSwimmersSubscription(FileHandling fileHandling) {
+        for (CompetitiveSwimmer competitiveSwimmer : fileHandling.getCompetitiveSwimmers()) {
+            System.out.println(competitiveSwimmer.getActivityType());
+            if (competitiveSwimmer.getActivityType()) {
+                int subscriptionFee = calculateSubscriptionFee(competitiveSwimmer);
+                competitiveSwimmer.setSubscriptionFee(subscriptionFee);
+            } else {
+                competitiveSwimmer.setSubscriptionFee(500);
+            }
+        }
+    }
 }
