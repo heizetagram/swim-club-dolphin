2a66c24cf32bfec5c753d1cbab295eb6db3015d3
David Lu <davidlu1999@gmail.com>
2023-12-07 12:40:17 +0100

    added printSwimmersInDebt(), printSwimmersInDebtInfo(), printCompetitiveSwimmersInDebtInfo(), printDueToBePaid()

--- printinfo/PrintInfo.java
+++ printinfo/PrintInfo.java
@@ -25,7 +25,7 @@ public class PrintInfo {
     private void printCompetitiveSwimmerInfo(CompetitiveSwimmer competitiveSwimmer) {
         UI.println(competitiveSwimmer.getName());
         UI.printf("   %s%-13s%s %s\n", ConsoleColors.GREEN_BRIGHT, "Team:", ConsoleColors.RESET, competitiveSwimmer.getAgeGroup());
-        UI.printf("   %s%-13s%s %s\n", ConsoleColors.GREEN_BRIGHT,  "Discipline:", ConsoleColors.RESET, competitiveSwimmer.getDiscipline());
+        UI.printf("   %s%-13s%s %s\n", ConsoleColors.GREEN_BRIGHT, "Discipline:", ConsoleColors.RESET, competitiveSwimmer.getDiscipline());
         UI.printf("   %s%-13s%s %s\n", ConsoleColors.GREEN_BRIGHT, "Pers. Record:", ConsoleColors.RESET, competitiveSwimmer.getSwimTime());
         UI.printf("   %s%-13s%s %s\n", ConsoleColors.GREEN_BRIGHT, "Competition:", ConsoleColors.RESET, competitiveSwimmer.getEvent());
         UI.printf("   %s%-13s%s %s\n", ConsoleColors.GREEN_BRIGHT, "Position:", ConsoleColors.RESET, competitiveSwimmer.getPosition());
@@ -74,5 +74,47 @@ public class PrintInfo {
                 counter++;
             }
         }
+    }
+
+    // Prints all swimmers in debt
+    public void printSwimmersInDebt(FileHandling fileHandling) {
+        UI.println(ConsoleColors.BLUE_BOLD + "Regular swimmers:" + ConsoleColors.RESET);
+        for (Swimmer swimmer : fileHandling.getSwimmers()) {
+            if (swimmer.getSubscriptionFee() > 0) {
+                printSwimmersInDebtInfo(swimmer);
+            }
+        }
+        UI.println(ConsoleColors.GREEN_BOLD_BRIGHT + "\nCompetitive swimmers:" + ConsoleColors.RESET);
+        for (CompetitiveSwimmer competitiveSwimmer : fileHandling.getCompetitiveSwimmers()) {
+            if (competitiveSwimmer.getSubscriptionFee() > 0) {
+                printCompetitiveSwimmersInDebtInfo(competitiveSwimmer);
+            }
+        }
+    }
+
+    // Print regular swimmers in debt info
+    private void printSwimmersInDebtInfo(Swimmer swimmer) {
+        // if hasPaid.equals("false")
+        UI.println(swimmer.getName());
+        UI.printf("   %s%-13s%s +45 %s\n", ConsoleColors.BLUE, "Phone:", ConsoleColors.RESET, swimmer.getPhone());
+        UI.printf("   %s%-13s%s %s\n", ConsoleColors.BLUE, "E-mail:", ConsoleColors.RESET, swimmer.getEmail());
+        UI.printf("   %s%-13s%s %s\n", ConsoleColors.BLUE, "Age group:", ConsoleColors.RESET, swimmer.getAgeGroup());
+        printDueToBePaid(swimmer.getSubscriptionFee());
+        UI.println("----------------------------------");
+    }
+
+    // Prints competitive swimmers in debt info
+    private void printCompetitiveSwimmersInDebtInfo(CompetitiveSwimmer competitiveSwimmer) {
+        UI.println(competitiveSwimmer.getName());
+        UI.printf("   %s%-13s%s +45 %s\n", ConsoleColors.GREEN_BRIGHT, "Phone:", ConsoleColors.RESET, competitiveSwimmer.getPhone());
+        UI.printf("   %s%-13s%s %s\n", ConsoleColors.GREEN_BRIGHT, "E-mail:", ConsoleColors.RESET, competitiveSwimmer.getEmail());
+        UI.printf("   %s%-13s%s %s\n", ConsoleColors.GREEN_BRIGHT, "Age group:", ConsoleColors.RESET, competitiveSwimmer.getAgeGroup());
+        printDueToBePaid(competitiveSwimmer.getSubscriptionFee());
+        UI.println("----------------------------------");
+    }
+
+    // Print subscription value to be paid
+    private void printDueToBePaid(int subscriptionFee) {
+        UI.printf("   %s%-13s%s %d%s\n", ConsoleColors.YELLOW, "To be paid:", ConsoleColors.RED_BOLD, subscriptionFee, ConsoleColors.RESET);
     }
 }
