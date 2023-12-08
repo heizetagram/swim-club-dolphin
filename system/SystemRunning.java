3302e95ad7c0d2d7ac3505b58fd34114968b15ea
David Lu <davidlu1999@gmail.com>
2023-12-08 16:32:20 +0100

    Added comments

--- system/SystemRunning.java
+++ system/SystemRunning.java
@@ -3,10 +3,12 @@ package system;
 public class SystemRunning {
     private static boolean isRunning;
 
+    // Getter
     public static boolean isRunning() {
         return isRunning;
     }
 
+    // Setter
     public static void setRunning(boolean isRunning) {
         SystemRunning.isRunning = isRunning;
     }
