a847747630684e0e6218af0a66c0d0cd36b7ee28
David Lu <davidlu1999@gmail.com>
2023-12-08 02:52:55 +0100

    Added FileHandling in constructor and added colorActivityType()

--- printinfo/PrintInfo.java
+++ printinfo/PrintInfo.java
@@ -8,8 +8,13 @@ import ui.SystemMessages;
 import ui.UI;
 
 public class PrintInfo {
+    private FileHandling fileHandling;
+
+    public PrintInfo(FileHandling fileHandling) {
+        this.fileHandling = fileHandling;
+    }
     // Prints all competitive swimmers
-    public void printAllCompetitiveSwimmers(FileHandling fileHandling) {
+    public void printAllCompetitiveSwimmers() {
         if (fileHandling.getCompetitiveSwimmers().isEmpty()) {
             SystemMessages.printRedColoredText("No competitive swimmers available");
         } else {
@@ -33,18 +38,18 @@ public class PrintInfo {
     }
 
     // Prints out all regular swimmers
-    public void printAllRegularSwimmers(FileHandling fileHandling){
+    public void printAllRegularSwimmers(){
         if (fileHandling.getSwimmers().isEmpty()) {
             SystemMessages.printRedColoredText("No swimmers available");
         } else {
             UI.println("Regular swimmers:");
             UI.println("----------------------------------");
-            printRegularSwimmerInfo(fileHandling);
+            printRegularSwimmerInfo();
         }
     }
 
     // Prints all regular swimmers' info
-    private void printRegularSwimmerInfo(FileHandling fileHandling) {
+    private void printRegularSwimmerInfo() {
         for (Swimmer swimmer : fileHandling.getSwimmers()) {
             UI.println(swimmer.getName());
             UI.printf("   %s%-13s%s %s\n", ConsoleColors.BLUE, "Birthdate:", ConsoleColors.RESET, swimmer.getBirthdate());
@@ -57,7 +62,7 @@ public class PrintInfo {
     }
 
     // Print top 5 competitive swimmers for each Discipline
-    public void printTop5CompetitiveSwimmers(FileHandling fileHandling) {
+    public void printTop5CompetitiveSwimmers() {
         String currentDiscipline = "";
         int counter = 0;
 
@@ -77,7 +82,7 @@ public class PrintInfo {
     }
 
     // Prints all swimmers in debt
-    public void printSwimmersInDebt(FileHandling fileHandling) {
+    public void printSwimmersInDebt() {
         UI.println(ConsoleColors.BLUE_BOLD + "Regular swimmers:" + ConsoleColors.RESET);
         for (Swimmer swimmer : fileHandling.getSwimmers()) {
             if (swimmer.getSubscriptionFee() > 0) {
@@ -99,6 +104,7 @@ public class PrintInfo {
         UI.printf("   %s%-13s%s +45 %s\n", ConsoleColors.BLUE, "Phone:", ConsoleColors.RESET, swimmer.getPhone());
         UI.printf("   %s%-13s%s %s\n", ConsoleColors.BLUE, "E-mail:", ConsoleColors.RESET, swimmer.getEmail());
         UI.printf("   %s%-13s%s %s\n", ConsoleColors.BLUE, "Age group:", ConsoleColors.RESET, swimmer.getAgeGroup());
+        UI.printf("   %s%-13s%s %s\n", ConsoleColors.BLUE, "Status:", ConsoleColors.RESET, colorActivityType(swimmer.getActivityType()));
         printDueToBePaid(swimmer.getSubscriptionFee());
         UI.println("----------------------------------");
     }
@@ -109,8 +115,19 @@ public class PrintInfo {
         UI.printf("   %s%-13s%s +45 %s\n", ConsoleColors.GREEN_BRIGHT, "Phone:", ConsoleColors.RESET, competitiveSwimmer.getPhone());
         UI.printf("   %s%-13s%s %s\n", ConsoleColors.GREEN_BRIGHT, "E-mail:", ConsoleColors.RESET, competitiveSwimmer.getEmail());
         UI.printf("   %s%-13s%s %s\n", ConsoleColors.GREEN_BRIGHT, "Age group:", ConsoleColors.RESET, competitiveSwimmer.getAgeGroup());
+        UI.printf("   %s%-13s%s %s\n", ConsoleColors.GREEN_BRIGHT, "Status:", ConsoleColors.RESET, colorActivityType(competitiveSwimmer.getActivityType()));
         printDueToBePaid(competitiveSwimmer.getSubscriptionFee());
         UI.println("----------------------------------");
+    }
+
+    // Color activity type
+    private String colorActivityType(String activityType) {
+        String coloredActivity = "";
+        switch (activityType) {
+          case "ACTIVE" -> coloredActivity = ConsoleColors.GREEN + "ACTIVE" + ConsoleColors.RESET;
+          case "PASSIVE" -> coloredActivity = ConsoleColors.RED + "PASSIVE" + ConsoleColors.RESET;
+        }
+        return coloredActivity;
     }
 
     // Print subscription value to be paid
