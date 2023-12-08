78e923c015edb73f1a5f1218107de6b75ba4376e
David Lu <davidlu1999@gmail.com>
2023-12-08 02:51:10 +0100

    Creates LoadAge

---
+++ filehandling/LoadAge.java
@@ -0,0 +1,20 @@
+package filehandling;
+
+import swimmer.CompetitiveSwimmer;
+import swimmer.Swimmer;
+
+public class LoadAge {
+    // Loads age and sets age in ArrayLists + .txt files
+    // -- To account for someone's birthday when their age is updated
+    public static void loadAge(FileHandling fileHandling) {
+        for (Swimmer swimmer : fileHandling.getSwimmers()) {
+            swimmer.calculateAgeAndAgeGroup();
+        }
+        for (CompetitiveSwimmer competitiveSwimmer : fileHandling.getCompetitiveSwimmers()) {
+            competitiveSwimmer.calculateAgeAndAgeGroup();
+        }
+
+        fileHandling.saveSwimmerToFile();
+        fileHandling.saveCompetitiveSwimmerToFile();
+    }
+}
